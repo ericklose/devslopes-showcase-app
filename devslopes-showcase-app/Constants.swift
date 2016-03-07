61e18f10c449dbdf8f7650f99027fb8454cc7804
Eric Klose <ericklose@gmail.com>
2016-03-07 12:27:43 -0600

    initial commit partway through first video

---
+++ devslopes-showcase-app/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  devslopes-showcase-app
+//
+//  Created by Eric Klose on 3/7/16.
+//  Copyright © 2016 Eric Klose. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
+
+
