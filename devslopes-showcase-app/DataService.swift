faea9fee27a0570ee80022ac283be1d3b8694e36
Eric Klose <ericklose@gmail.com>
2016-03-09 15:20:07 -0700

    mostly firebase stuff with a bit of connecting the code

--- devslopes-showcase-app/DataService.swift
+++ devslopes-showcase-app/DataService.swift
@@ -9,13 +9,28 @@
 import Foundation
 import Firebase
 
+let URL_BASE = "https://learning-how.firebaseio.com"
 
 class DataService {
     static let ds = DataService()
     
-    private var _REF_BASE  = Firebase(url: "https://learning-how.firebaseio.com")
+    private var _REF_BASE = Firebase(url: "\(URL_BASE)")
+    private var _REF_POST = Firebase(url: "\(URL_BASE)/posts")
+    private var _REF_USERS = Firebase(url: "\(URL_BASE)/users")
     
     var REF_BASE: Firebase {
         return _REF_BASE
+    }
+    
+    var REF_POST: Firebase {
+        return _REF_POST
+    }
+    
+    var REF_USERS: Firebase {
+        return _REF_USERS
+    }
+    
+    func createFirebaseUser(uid: String, user: Dictionary<String, String>) {
+        REF_USERS.childByAppendingPath(uid).setValue(user)
     }
 }
